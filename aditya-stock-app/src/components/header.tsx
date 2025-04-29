c53cf22493a68b8685a6c7eb075a667c9e62055e
Aditya Dhananjay Singh <asingh772004@gmail.com>
2025-04-29 19:58:03 +0530

    Added icon to header using Lucide React

--- aditya-stock-app/src/components/header.tsx
+++ aditya-stock-app/src/components/header.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { TrendingUp } from 'lucide-react'; // Importing Lucide icon
 import '../layout.css';
 
 const Header: React.FC = () => (
-  <div className="header">
-    Aditya's Stock Visualizer
+  <div className="header flex items-center gap-2">
+    <TrendingUp size={32} className="text-green-500" /> {/* Adding the stock-related icon */}
+    <span>Aditya's Stock Visualizer</span>
   </div>
 );
 
